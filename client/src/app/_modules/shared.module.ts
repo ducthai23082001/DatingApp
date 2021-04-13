a94005270d7de43eb4d081ff1e28b37d393c4e2b
ducthai23082001 <62064071+ducthai23082001@users.noreply.github.com>
2021-04-13 11:50:36 +0700

    End of section 6

---
+++ client/src/app/_modules/shared.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown/bs-dropdown.module';
+import { ToastrModule } from 'ngx-toastr';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BsDropdownModule.forRoot(),
+    ToastrModule.forRoot({
+      positionClass: 'toast-bottom-right'
+    })
+  ]
+})
+export class SharedModule { }
